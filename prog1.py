5811e28a670bfa0cf2f6937937969ade3f7dcea1
SharkovKirill <kirill.office75@gmail.com>
2019-09-18 15:52:27 +0700

    Templates added

--- prog1.py
+++ prog1.py
@@ -1,38 +1,71 @@
 # Case-study #1
 # Developers:
 #
-#               
+#
 import turtle
 
+def paragram1(x,y,a,color):
+    #TODO:(Kirill) Function, drawing parallelogram1.
+    pass
 
-def square(x, y, a):
-    '''
-    Function, drawing square.
-    :param x: upper left corner coordinate x
-    :param y: upper left corner coordinate y
-    :param a: side length of a square
-    :return: None
-    '''
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
+def paragram2(x,y,a,color):
+    #TODO:(Kirill) Function, drawing parallelogram2.
+    pass
 
+def paragram3(x,y,a,color):
+    #TODO:(Kirill) Function, drawing parallelogram3.
+    pass
+
+def paragram4(x,y,a,color):
+    #TODO:(Kirill) Function, drawing parallelogram4.
+    pass
+
+def square1(x,y,a,color):
+    #TODO:(Kirill) Function, drawing square1.
+    pass
+
+def square2(x,y,a,color):
+    #TODO:(Kirill) Function, drawing square2.
+    pass
+
+def triangle1(x,y,a,color):
+    #TODO:(Sveta) Function, drawing triangle1.
+    pass
+
+def triangle2(x,y,a,color):
+    #TODO:(Sveta) Function, drawing triangle2.
+    pass
+
+def triangle3(x,y,a,color):
+    #TODO:(Sveta) Function, drawing triangle3.
+    pass
+
+def triangle4(x,y,a,color):
+    #TODO:(Sveta) Function, drawing triangle4.
+    pass
+
+def triangleleft(x,y,a,color):
+    #TODO:(Vova) Function, drawing triangle.
+    pass
+
+def triangleright(x,y,a,color):
+    #TODO:(Vova) Function, drawing triangle.
+    pass
+
+def triangleup(x,y,a,color):
+    #TODO:(Vova) Function, drawing triangle.
+    pass
+
+def triangledown(x,y,a,color):
+    #TODO:(Vova) Function, drawing triangle.
+    pass
+
+#TODO:(Kirill) Functions drawing pictures 1,2,3
+#TODO:(Sveta) Functions drawing pictures 4,5,6
+#TODO:(Vova) Functions drawing pictures 7,8,9
 
 def main():
     '''
     Main function.
     :return: None
     '''
-    square(-200, 200, 180)
-    square(20, 200, 180)
-    square(20, -20, 180)
-    square(-200, -20, 180)
-    turtle.done()
