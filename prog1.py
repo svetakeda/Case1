27226a52e90a5a207779df9276938ad8e793beda
svetockak <svetakeda2504@gmail.com>
2019-09-20 19:38:15 +0700

    added 4 triangles

--- prog1.py
+++ prog1.py
@@ -62,19 +62,72 @@ def square2(x,y,a,color):
 
 def triangle1(x,y,a,color):
     #TODO:(Sveta) Function, drawing triangle1.
-    ggg
+    turtle.fillcolor(color)
+    turtle.pencolor(color)
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.left(90)
+    turtle.forward(a)
+    turtle.left(90)
+    turtle.forward(a)
+    turtle.left(135)
+    turtle.forward(math.sqrt(2)*a)
+    turtle.right(135)
+    turtle.end_fill()
     pass
 
 def triangle2(x,y,a,color):
     #TODO:(Sveta) Function, drawing triangle2.
+    turtle.fillcolor(color)
+    turtle.pencolor(color)
+    turtle.up()
+    turtle.setposition(x,y)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.left(90)
+    turtle.forward(a)
+    turtle.left(135)
+    turtle.forward(math.sqrt(2) * a)
+    turtle.right(135)
+    turtle.end_fill()
     pass
 
 def triangle3(x,y,a,color):
     #TODO:(Sveta) Function, drawing triangle3.
+    turtle.fillcolor(color)
+    turtle.pencolor(color)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.setposition(x, y)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(math.sqrt(2) * a)
+    turtle.right(135)
+    turtle.end_fill()
     pass
 
 def triangle4(x,y,a,color):
     #TODO:(Sveta) Function, drawing triangle4.
+    turtle.fillcolor(color)
+    turtle.pencolor(color)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.setposition(x, y)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(math.sqrt(2) * a)
+    turtle.right(135)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.end_fill()
     pass
 
 def triangleleft(x,y,a,color):
@@ -103,4 +156,3 @@ def main():
     :return: None
     '''
 
-time.sleep(3)
